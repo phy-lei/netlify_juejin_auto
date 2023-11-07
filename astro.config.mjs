d6e1a929f49f494e605a83e18be9c24fc04ca4b9
phy-lei <765373325@qq.com>
2023-11-07 19:27:36 +0800

    feat: 新增nelify schedule

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,7 @@
 import { defineConfig } from 'astro/config';
 import 'dotenv/config';
-import cron from 'node-cron';
 import netlify from '@astrojs/netlify/functions';
 import node from '@astrojs/node';
-import autoProcess from './src/process/start';
-
-cron.schedule(process.env.CRON_EXPRESSION, () => {
-  autoProcess();
-});
 
 const envAdapter = () => {
   switch (process.env.OUTPUT) {
